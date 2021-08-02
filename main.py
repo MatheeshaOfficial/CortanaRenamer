27052fe0173c8d6c72e85f1c8b89d1efd7db6719
matheeshaofficial <77558226+matheeshaofficial@users.noreply.github.com>
2021-08-02 16:31:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ async def settings_handler(bot: Client, event: Message, ):
     await OpenSettings(editable, user_id=event.from_user.id)
 
 @Cortana.on_callback_query()
-async def callback_handlers(bot: Client, cb: CallbackQuery, message):
+async def callback_handlers(bot: Client, cb: CallbackQuery):
     if "closeMeh" in cb.data:
         await cb.message.delete(True)
     elif "openSettings" in cb.data:
