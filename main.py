698182f222b2e8e746f2b06913e90c571730cdc2
matheeshaofficial <77558226+matheeshaofficial@users.noreply.github.com>
2021-08-02 14:06:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -249,8 +249,14 @@ async def show_status_count(_, event: Message):
 
 @Cortana.on_message(filters.private & filters.command("settings"))
 async def settings_handler(bot: Client, event: Message):
+    await AddUserToDatabase(bot, event)
+    FSub = await ForceSub(bot, event)
+    if FSub == 400:
+        return
+    editable = await event.reply_text(
+        text="Please Wait ..."
+    )
     await OpenSettings(editable, user_id=event.from_user.id)
-
 
 @Cortana.on_callback_query()
 async def callback_handlers(bot: Client, cb: CallbackQuery):
