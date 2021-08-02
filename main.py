564e22ce6051b3576df6683e334e7fc2b98aec60
matheeshaofficial <77558226+matheeshaofficial@users.noreply.github.com>
2021-08-02 16:03:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ async def show_status_count(_, event: Message):
 
 
 @Cortana.on_message(filters.private & filters.command("settings"))
-async def settings_handler(bot: Client, event: Message):
+async def settings_handler(bot: Client, event: Message, ):
     await AddUserToDatabase(bot, event)
     FSub = await ForceSub(bot, event)
     if FSub == 400:
@@ -291,12 +291,12 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
             )
     elif "home" in cb.data:
         await cb.message.edit(
-            text=Translation.START_TEXT.format(cb.message.from_user.mention),
+            text=Translation.START_TEXT.format(event.message.from_user.mention),
             reply_markup=Translation.START_BUTTONS
         )
     elif "help" in cb.data:
         await cb.message.edit(
-            text=Translation.HELP_TEXT.format(cb.message.from_user.mention),
+            text=Translation.HELP_TEXT.format(event.message.from_user.mention),
             reply_markup=Translation.HELP_BUTTONS
         )
     elif "about" in cb.data:
@@ -311,7 +311,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         )
     elif "info" in cb.data:
         await cb.message.edit(
-            text=Translation.INFO_TEXT.format(username=cb.message.from_user.username, first_name=cb.message.from_user.first_name, last_name=cb.message.from_user.last_name, user_id=cb.message.from_user.id, mention=cb.message.from_user.mention),
+            text=Translation.INFO_TEXT.format(username=event.from_user.username, first_name=cb.message.from_user.first_name, last_name=cb.message.from_user.last_name, user_id=cb.message.from_user.id, mention=cb.message.from_user.mention),
             reply_markup=Translation.INFO_BUTTONS
         )
     elif "triggerPrefix" in cb.data:
